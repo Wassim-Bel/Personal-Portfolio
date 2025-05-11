51dd8295ca3bf52e6790cdf323f5c7786f3ea2fa
Wassim-Bel <wassimbelghache@gmail.com>
2025-05-10 21:29:08 -0600

    making edits to intro container

---
+++ main.js
@@ -0,0 +1,10 @@
+function copyEmail() {
+    const email = document.getElementById("email").innerText;
+    navigator.clipboard.writeText(email)
+      .then(() => {
+        alert("Email copied to clipboard!");
+      })
+      .catch(err => {
+        console.error("Failed to copy email: ", err);
+      });
+  }
