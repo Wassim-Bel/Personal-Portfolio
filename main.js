d5cd89c7de4c999ff8b32e02a025772d5fc6123a
Wassim-Bel <wassimbelghache@gmail.com>
2025-05-13 14:26:28 -0600

    adding animation, text copy css, and minor changes

--- main.js
+++ main.js
@@ -2,9 +2,17 @@ function copyEmail() {
     const email = document.getElementById("email").innerText;
     navigator.clipboard.writeText(email)
       .then(() => {
-        alert("Email copied to clipboard!");
+        const notif = document.getElementById("copy-notification");
+        notif.classList.remove("hidden");
+        notif.classList.add("show");
+
+        // Hide after 2 seconds
+        setTimeout(() => {
+          notif.classList.remove("show");
+          notif.classList.add("hidden");
+        }, 2000);
       })
       .catch(err => {
         console.error("Failed to copy email: ", err);
       });
-  }
+}
