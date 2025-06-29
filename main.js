4a2ec3708deb23500214a901d4335d5ff7ee7e2d
Wassim-Bel <wassimbelghache@gmail.com>
2025-06-29 20:52:30 +0300

    making tabs interactive

--- main.js
+++ main.js
@@ -16,3 +16,30 @@ function copyEmail() {
         console.error("Failed to copy email: ", err);
       });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const tab1 = document.querySelector(".tab-1");
+  const tab2 = document.querySelector(".tab-2");
+  const skillsContent = document.getElementById("skills-content");
+  const educationContent = document.getElementById("education-content");
+
+  function activateTab(tabName) {
+      if (tabName === "skills") {
+          tab1.classList.add("active-tab");
+          tab2.classList.remove("active-tab");
+          skillsContent.classList.remove("hidden");
+          educationContent.classList.add("hidden");
+      } else {
+          tab2.classList.add("active-tab");
+          tab1.classList.remove("active-tab");
+          educationContent.classList.remove("hidden");
+          skillsContent.classList.add("hidden");
+      }
+  }
+
+  // Default to 'skills'
+  activateTab("skills");
+
+  tab1.addEventListener("click", () => activateTab("skills"));
+  tab2.addEventListener("click", () => activateTab("education"));
+});
